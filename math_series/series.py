e80e07802bb21cd579a1d1cd8a0c21567a8c5c71
Mani Kondapaneni <kondapan@amazon.com>
2020-04-17 15:01:04 -0700

    Completed lucas function

--- math_series/series.py
+++ math_series/series.py
@@ -3,3 +3,11 @@ def fibonacci(n):
         return n
     return fibonacci(n-1)+fibonacci(n-2)
 
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    return lucas(n-1)+lucas(n-2)
+
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,6 @@
 from math_series.series import fibonacci
+from math_series.series import lucas
+
 def test_0():
     fb = fibonacci(0)
     assert 0 == fb
@@ -28,4 +30,31 @@ def test_6():
     assert 8 == fb
 
 
+def test_l_0():
+    fb = lucas(0)
+    assert 2 == fb
+
+def test_l_1():
+    fb = lucas(1)
+    assert 1 == fb
+
+def test_l_2():
+    fb = lucas(2)
+    assert 3 == fb
+
+def test_l_3():
+    fb = lucas(3)
+    assert 4 == fb
+
+def test_l_4():
+    fb = lucas(4)
+    assert 7 == fb
+
+def test_l_5():
+    fb = lucas(5)
+    assert 11 == fb
+
+def test_l_6():
+    fb = lucas(6)
+    assert 18 == fb
 

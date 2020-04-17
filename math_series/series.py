d862b354864f82e8d0a8cffa9390a71320888fac
Mani Kondapaneni <kondapan@amazon.com>
2020-04-17 14:53:43 -0700

    Completed Fibonacci

--- math_series/series.py
+++ math_series/series.py
@@ -1,2 +1,5 @@
 def fibonacci(n):
-    return n
+    if n < 2:
+        return n
+    return fibonacci(n-1)+fibonacci(n-2)
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,31 @@
 from math_series.series import fibonacci
 def test_0():
     fb = fibonacci(0)
-    assert 0 == 0
+    assert 0 == fb
+
+def test_1():
+    fb = fibonacci(1)
+    assert 1 == fb
+
+def test_2():
+    fb = fibonacci(2)
+    assert 1 == fb
+
+def test_3():
+    fb = fibonacci(3)
+    assert 2 == fb
+
+def test_4():
+    fb = fibonacci(4)
+    assert 3 == fb
+
+def test_5():
+    fb = fibonacci(5)
+    assert 5 == fb
+
+def test_6():
+    fb = fibonacci(6)
+    assert 8 == fb
+
+
+
